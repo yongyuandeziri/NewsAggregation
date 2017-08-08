617fbd4fa916345cd45d48c46540e0cfb307fb7e
ward_du <Ward_Du@asus.com>
2017-08-08 13:43:37 +0800

    add more newssource from different country

--- app/src/main/java/cn/news/ziri/newsaggregation/sqlite/NewsSourceSQLiteOpenHelper.java
+++ app/src/main/java/cn/news/ziri/newsaggregation/sqlite/NewsSourceSQLiteOpenHelper.java
@@ -55,6 +55,25 @@ public class NewsSourceSQLiteOpenHelper extends SQLiteOpenHelper {
         db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"参考消息\",\"http://m.cankaoxiaoxi.com/\",0,1)");
         db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"南方周末\",\"http://m.infzm.com/\",0,1)");
         db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"经济日报\",\"http://m.ce.cn/yw/\",0,1)");
+
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"BBC\",\"www.bbc.com\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"腾讯新闻\",\"http://xw.qq.com/m/news/index.html\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"新浪新闻\",\"https://news.sina.cn/?from=wap\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"金融时报\",\"www.ftchinese.com/channel/china.html\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"俄罗斯卫星通讯社\",\"sputniknews.cn/china/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"俄塔社\",\"tass.com\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"NHK World\",\"https://www3.nhk.or.jp/nhkworld/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"半岛电视台\",\"www.aljazeera.com\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"哥伦比亚新闻\",\"http://www.cbsnews.com/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"ABC News\",\"http://abcnews.go.com/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"NBC News\",\"http://www.nbcnews.com/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"FOX News\",\"http://www.foxnews.com/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"CNN News\",\"http://edition.cnn.com/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"NPR News\",\"http://www.npr.org/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"赫芬顿邮报\",\"http://www.huffingtonpost.com/\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"新加坡联合早报\",\"http://www.zaobao.com/news\",0,1)");
+        db.execSQL("insert into newssource (name,uri,isneedjson,isselected) values(\"USA Today\",\"https://www.usatoday.com/news/\",0,1)");
+
         db.setTransactionSuccessful();
         db.endTransaction();
     }
